ac82e7ab945f543ec6fda1ed2070203189af96d9
Matias Tortoriello <matiastortoriello1991@gmail.com>
2022-10-28 20:00:51 -0300

    Arranca la lógica JavaScript

---
+++ script/catalogoJuegos.js
@@ -0,0 +1,12 @@
+let listaJuegos = [
+    
+    {
+        id: 01,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    }
+    
+]
