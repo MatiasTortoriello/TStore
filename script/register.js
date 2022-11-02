8b0d8121c96b426bb04fb3de5e16c8ad43374d82
Matias Tortoriello <matiastortoriello1991@gmail.com>
2022-11-02 18:08:23 -0300

    .

--- script/register.js
+++ script/register.js
@@ -1,1 +1,31 @@
-let registro = document.getElementById
+let registro = document.getElementById('registro');
+
+registro.addEventListener('click', (e) =>{
+
+    e.preventDefault()
+    if(validar()){
+        regsitrarCuenta();
+    } else {
+        prompt('No se pudo registrar la cuenta.')
+    }
+    
+})
+
+function validar(){
+    let error = false;
+    let mensajeError = "";
+    
+    const nombreRegistro = document.getElementById('nombreUsuarioRegistro');
+    const password = document.getElementById('passwordUsuarioRegistro')
+    const passwordRepetida = document.getElementById('passwordUsuarioRegistroDos')
+    const email = document.getElementById('emailUsuarioRegistro')
+
+    if(nombreRegistro.value=""){
+        error = true;
+        mensajeError+="El nombre tiene que tener un mínimo de 5 caracteres"
+    }
+}
+
+function regsitrarCuenta(){
+
+}
