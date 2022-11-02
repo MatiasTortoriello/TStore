547a6bc568ec8c81a1df18d4813eb398a5c5eac3
Matias Tortoriello <matiastortoriello1991@gmail.com>
2022-11-02 17:40:50 -0300

    .

--- script/catalogoJuegos.js
+++ script/catalogoJuegos.js
@@ -7,6 +7,86 @@ let listaJuegos = [
         plataformas: ["PC", "Playstation", "Xbox"],
         precio: 7000,
         descripcion: "Un juego re jodido. Compralo si tenes huevos."
-    }
+    },
+
+    {
+        id: 02,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 03,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 04,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 05,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 06,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 07,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 08,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
     
+    {
+        id: 08,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
+
+    {
+        id: 08,
+        nombre: "Dark Souls",
+        genero: "Rpg",
+        plataformas: ["PC", "Playstation", "Xbox"],
+        precio: 7000,
+        descripcion: "Un juego re jodido. Compralo si tenes huevos."
+    },
 ]
