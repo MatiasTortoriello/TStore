0647a59c871ace54a9ab255f0e4874f1e0ffda4f
Matias Tortoriello <matiastortoriello1991@gmail.com>
2022-11-04 20:19:19 -0300

    Registro Regex

--- script/register.js
+++ script/register.js
@@ -1,62 +1,39 @@
-let registro = document.getElementById('registro');
-let regexPasswordInvalid = / /;
+let registro = document.querySelector('#form');
+let regexPasswordInvalid = /^(([A-Z])+\w+([0-9])+)+\w+$/;
 let regexEmail=/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[a-zA-Z]+$/;
+
+let nombreRegistro = document.getElementById('nombreUsuarioRegistro');
+let passwordRegistro = document.getElementById('passwordUsuarioRegistro')
+let passwordRepetida = document.getElementById('passwordUsuarioRegistroDos')
+let emailRegistro = document.getElementById('emailUsuarioRegistro')
 
 let usuariosRegistrados=[]
 
-registro.addEventListener('click', (e) =>{
+registro.addEventListener('submit', (e) =>{
 
     e.preventDefault()
-    if(validar()){
-        regsitrarCuenta();
-    } else {
-        alert('No se pudo registrar la cuenta.')
-    }
-    
-})
-
-function validar(){
     let error = false;
     let mensajeError = "";
-    let regexPasswordInvalid = / /;
-    let regexEmail=/^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[a-zA-Z]+$/;
     
-    const nombreRegistro = document.getElementById('nombreUsuarioRegistro');
-    const passwordRegistro = document.getElementById('passwordUsuarioRegistro')
-    const passwordRepetida = document.getElementById('passwordUsuarioRegistroDos')
-    const emailRegistro = document.getElementById('emailUsuarioRegistro')
-
     if(nombreRegistro.value.length<5){
         error = true;
         mensajeError+="<p>El nombre tiene que tener un mínimo de 5 caracteres</p>"
         alert('El nombre tiene que tener un mínimo de 5 caracteres')
     }
 
-    if(passwordRegistro.length<5){
+    if(passwordRegistro.length<=7){
         error=true
         console.log(error)
-        alert('La contraseña debe tener más de 5 caracteres')
+        alert('La contraseña debe tener al menos 8 caracteres')
     }
 
-    if(!passwordRegistro.test(/^[A-Z]$/)){
+    if(!regexPasswordInvalid.test(passwordRegistro)){
         error=true
         console.log(error)
-        alert('La contraseña debe tener una letra mayúscula')
-    }
+        alert('La contraseña debe comenzar con mayúscula y tener al menos un número')
+    } 
 
-    /*if(!passwordRegistro.matches(/\d/)){
-        error=true
-        console.log(error)
-        alert('La contraseña debe tener al menos un número')
-    }*/
-
-    /*if(regexPasswordInvalid.test(passwordRegistro)){
-        error = true
-        console.log(error)
-        alert('la contraseña no debe contener espacios')
-    }*/
-
-    if(passwordRepetida.length !== passwordRegistro.length){
+    if(passwordRepetida.textContent !== passwordRegistro.textContent){
         error = true
         console.log(error)
         alert('Las contraseñas no coinciden')
@@ -68,7 +45,13 @@ function validar(){
         alert('El email es incorrecto')
     }
 
-}
+    if(error){
+        alert('No se mandó nada')
+    }else{
+        form.submit();
+        regsitrarCuenta();
+    }
+})
 
 function regsitrarCuenta(){
     
@@ -85,5 +68,6 @@ function regsitrarCuenta(){
 
 function usuarioLocalStorage(){
     localStorage.setItem("Usuarios", "Cuentas")
+
     localStorage.setItem("Usuarios",JSON.stringify(usuariosRegistrados))
 }
